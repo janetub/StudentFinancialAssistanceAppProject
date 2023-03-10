81e30fed67689feb56442193ee041e365445581a
Niña Jane Tubigon <21-1-00632@vsu.edu.ph>
2023-03-10 14:23:36 +0800

    Add project files.

---
+++ StudentFinancialAssistanceAppProject/Form1.cs
@@ -0,0 +1,10 @@
+namespace StudentFinancialAssistanceAppProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
